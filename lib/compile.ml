0b24a90c82ddafce53877f9e8453df51cf1c064f
Rob Lewis <Rob.y.lewis@gmail.com>
2023-09-12 13:59:11 -0400

    improve asm library

--- lib/compile.ml
+++ lib/compile.ml
@@ -1,16 +1,24 @@
-let compile (program : string) : string =
-  String.concat "\n"
-    ["global entry"; "entry:"; Printf.sprintf "\tmov rax, %s" program; "\tret"]
+open S_exp
+
+exception BadExpression of s_exp
+
+let compile (program : s_exp) : string =
+  match program with
+  | Num n ->
+      String.concat "\n"
+        ["global entry"; "entry:"; Printf.sprintf "\tmov rax, %d" n; "\tret"]
+  | e ->
+      raise (BadExpression e)
 
 let compile_to_file (program : string) : unit =
   let file = open_out "program.s" in
-  output_string file (compile program) ;
+  output_string file (compile (parse program)) ;
   close_out file
 
 let compile_and_run (program : string) : string =
   compile_to_file program ;
   ignore (Unix.system "nasm program.s -f elf64 -o program.o") ;
-  ignore (Unix.system "gcc program.o runtime.o -o program") ;
+  ignore (Unix.system "gcc program.o runtime.o -o program -z noexecstack") ;
   let inp = Unix.open_process_in "./program" in
   let r = input_line inp in
   close_in inp ; r
