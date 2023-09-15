ade7be691fec3cf5f4553aafbf45c46d4d4038f0
Rob Lewis <Rob.y.lewis@gmail.com>
2023-09-15 13:42:51 -0400

    after class Sep 13

--- lib/compile.ml
+++ lib/compile.ml
@@ -1,14 +1,23 @@
 open S_exp
+open Asm
 
 exception BadExpression of s_exp
 
-let compile (program : s_exp) : string =
-  match program with
+let rec compile_exp (exp : s_exp) : directive list =
+  match exp with
   | Num n ->
-      String.concat "\n"
-        ["global entry"; "entry:"; Printf.sprintf "\tmov rax, %d" n; "\tret"]
+      [Mov (Reg Rax, Imm n)]
+  | Lst [Sym "add1"; arg] ->
+      compile_exp arg @ [Add (Reg Rax, Imm 1)]
+  | Lst [Sym "sub1"; arg] ->
+      compile_exp arg @ [Sub (Reg Rax, Imm 1)]
   | e ->
       raise (BadExpression e)
+
+let compile (program : s_exp) : string =
+  [Global "entry"; Label "entry"] @ compile_exp program @ [Ret]
+  |> List.map string_of_directive
+  |> String.concat "\n"
 
 let compile_to_file (program : string) : unit =
   let file = open_out "program.s" in
@@ -18,7 +27,30 @@ let compile_to_file (program : string) : unit =
 let compile_and_run (program : string) : string =
   compile_to_file program ;
   ignore (Unix.system "nasm program.s -f elf64 -o program.o") ;
-  ignore (Unix.system "gcc program.o runtime.o -o program -z noexecstack") ;
+  ignore
+    (Unix.system "gcc program.o runtime.o -o program -z noexecstack") ;
   let inp = Unix.open_process_in "./program" in
   let r = input_line inp in
   close_in inp ; r
+
+let rec interp_exp (exp : s_exp) : int =
+  match exp with
+  | Num n ->
+      n
+  | Lst [Sym "add1"; arg] ->
+      interp_exp arg + 1
+  | Lst [Sym "sub1"; arg] ->
+      interp_exp arg - 1
+  | _ ->
+      raise (BadExpression exp)
+
+let interp (program : string) : string =
+  parse program |> interp_exp |> string_of_int
+
+let difftest (examples : string list) =
+  let results =
+    List.map (fun ex -> (compile_and_run ex, interp ex)) examples
+  in
+  List.for_all (fun (r1, r2) -> r1 = r2) results
+
+let test () = difftest ["43"; "(add1 (add1 3))"; "(sub1 4)"]
