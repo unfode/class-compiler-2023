8b10053cb4ed14d055d134969a4a0954940d4dd1
Futao Wei <95689995+unfode@users.noreply.github.com>
2023-11-08 11:29:41 -0500

    Add `reference_type_shift`

--- lib/encode.ml
+++ lib/encode.ml
@@ -1,18 +1,17 @@
 type value_type_spec = {shift: int; tag: int}
 
 let num_spec : value_type_spec = {shift = 2; tag = 0b00}
-
 let bool_spec : value_type_spec = {shift= 7; tag= 0b0011111}
-
-type reference_type_spec = {tag: int}
-
-let pair_spec : reference_type_spec = {tag = 0b010}
-
-let function_spec : reference_type_spec = {tag = 0b110}
 
 let get_mask (spec : value_type_spec) : int = (1 lsl spec.shift) - 1
 
+type reference_type_spec = {tag: int}
+
 let reference_type_mask : int = 0b111
+let reference_type_shift : int = 3
+
+let pair_spec : reference_type_spec = {tag = 0b010}
+let function_spec : reference_type_spec = {tag = 0b110}
 
 let encode (value : int) (spec : value_type_spec) : int =
   (value lsl spec.shift) lor spec.tag
